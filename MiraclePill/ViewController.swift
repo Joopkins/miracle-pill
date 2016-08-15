a0abca7c309409320f834f3e843204e32db936b5
Joopkins <jobndingdier@gmail.com>
2016-08-15 19:35:35 -0400

    feat: Add country and zip fields & buy now button

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var street: UILabel!
+    @IBOutlet weak var streetField: UITextField!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var state: UILabel!
+    @IBOutlet weak var zip: UILabel!
+    @IBOutlet weak var zipField: UITextField!
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var countryField: UITextField!
     @IBOutlet weak var statePicker: UIPickerView!
     @IBOutlet weak var stateButton: UIButton!
+    @IBOutlet weak var buyNowButton: UIButton!
+    @IBOutlet weak var succesImage: UIImageView!
     
     let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
     
@@ -26,8 +39,31 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func buyNowButtonPressed(_ sender: AnyObject) {
+        succesImage.isHidden = false
+        buyNowButton.isHidden = true
+        stateButton.isHidden = true
+        statePicker.isHidden = true
+        state.isHidden = true
+        name.isHidden = true
+        street.isHidden = true
+        city.isHidden = true
+        nameField.isHidden = true
+        streetField.isHidden = true
+        cityField.isHidden = true
+        zip.isHidden = true
+        zipField.isHidden = true
+        country.isHidden = true
+        countryField.isHidden = true
+        
+    }
+    
     @IBAction func stateButtonPressed(_ sender: AnyObject) {
         statePicker.isHidden = false
+        zip.isHidden = true
+        zipField.isHidden = true
+        country.isHidden = true
+        countryField.isHidden = true
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -45,6 +81,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         stateButton.setTitle(states[row], for: UIControlState.normal)
         statePicker.isHidden = true
+        zip.isHidden = false
+        zipField.isHidden = false
+        country.isHidden = false
+        countryField.isHidden = false
     }
 }
 
